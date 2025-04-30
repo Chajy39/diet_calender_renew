1fe82506b6b276fdc77dbb76f1217ce7ce2c24dc
Chajy <wlsdud3957@ivid.kr>
2025-04-30 17:42:09 +0900

    tanstack-query 적용 / bottomTabs a태그 Link로 변경

--- src/app/BottomTabs.tsx
+++ src/app/BottomTabs.tsx
@@ -1,24 +1,61 @@
+"use client";
+import { useQueryClient } from "@tanstack/react-query";
 import tw from "tailwind-styled-components";
+import Link from "next/link";
+import Image, { ImageProps } from "next/image";
 
 const FooterWrap = tw.footer`fixed w-full b-0 l-0`;
 const BottomTabWrap = tw.nav`flex justify-between items-center gap-6`;
-const TabItem = tw.a`flex-1 flex flex-col items-center gap-1 text-[0.75rem] text-[#555555] text-semibold py-2`;
-const TabImg = tw.img`w-5 h-5`;
+const TabItemWrap = tw.div`flex-1`;
+const TabItem = tw.a`flex flex-col items-center gap-1 text-[0.75rem] text-[#555555] text-semibold py-2`;
+const TabImg = tw(Image)<ImageProps>``;
 
 const BottomTabs = () => {
+  const queryClient = useQueryClient();
+  const auth = queryClient.getQueryData(["auth"]);
+
+  const getUserLink = () => {
+    console.log("getUserLink", auth ? "/user" : "/login");
+    return auth ? "/user" : "/login";
+  };
+
   return (
     <FooterWrap>
       <BottomTabWrap>
-        <TabItem href="/home">
-          <TabImg src="/svg/ic_home.svg" alt="home" />홈
-        </TabItem>
-        <TabItem href="/tip">
-          <TabImg src="/svg/ic_tip.svg" alt="home" />
-          꿀팁
-        </TabItem>
-        <TabItem href="/user">
-          <TabImg src="/svg/ic_user.svg" alt="home" />내 정보
-        </TabItem>
+        <TabItemWrap>
+          <Link href="/home" passHref>
+            <TabItem>
+              <TabImg
+                src="/svg/ic_home.svg"
+                alt="home"
+                width={24}
+                height={24}
+              />
+              홈
+            </TabItem>
+          </Link>
+        </TabItemWrap>
+        <TabItemWrap>
+          <Link href="/tip" passHref>
+            <TabItem>
+              <TabImg src="/svg/ic_tip.svg" alt="home" width={24} height={24} />
+              꿀팁
+            </TabItem>
+          </Link>
+        </TabItemWrap>
+        <TabItemWrap>
+          <Link href={getUserLink()} passHref>
+            <TabItem>
+              <TabImg
+                src="/svg/ic_user.svg"
+                alt="home"
+                width={24}
+                height={24}
+              />
+              내 정보
+            </TabItem>
+          </Link>
+        </TabItemWrap>
       </BottomTabWrap>
     </FooterWrap>
   );
