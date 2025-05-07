ca92025a203e938d19d65fce05d400704e2a22bc
cjy <wlsdud3957@naver.com>
2025-05-07 23:48:58 +0900

    todayChart

--- src/app/home/TodayChart.tsx
+++ src/app/home/TodayChart.tsx
@@ -7,11 +7,17 @@
 
 const TodayChart = () => {
   return (
-    <div className="flex gap-4">
-      <div className="rounded-full"></div>
-      <div className="flex flex-col gap-2">
-        <div className="relative before:content-['탄'] before:absolute before:rounded-full before:bg-red">
-          <div className={`absolute w-[50%] h-full bg-red`} />
+    <div className="flex gap-10 px-6">
+      <div className="relative w-30 h-30 rounded-full bg-[#cecece] after:content[''] after:w-16 after:h-16 after:absolute after:left-7 after:top-7 after:bg-white after:rounded-full"></div>
+      <div className="flex flex-col justify-center gap-2">
+        <div className="relative before:content-['탄'] w-30 h-6 before:absolute before:rounded-full before:bg-red before:left-[-20px]">
+          <div className={`absolute w-full h-full bg-[#ff0000] rounded-sm`} />
+        </div>
+        <div className="relative before:content-['단'] w-30 h-6 before:absolute before:rounded-full before:bg-red before:left-[-20px]">
+          <div className={`absolute w-full h-full bg-[#00ff00] rounded-sm`} />
+        </div>
+        <div className="relative before:content-['지'] w-30 h-6 before:absolute before:rounded-full before:bg-red before:left-[-20px]">
+          <div className={`absolute w-full h-full bg-[#0000ff] rounded-sm`} />
         </div>
       </div>
     </div>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
 import AddButton from "./AddButton";
 import CalendarHeader from "./CalendarHeader";
+import TodayChart from "./TodayChart";
 
 const Home = () => {
   return (
     <div className="w-full h-full relative">
       <CalendarHeader />
+      <TodayChart />
       <AddButton />
     </div>
   );
